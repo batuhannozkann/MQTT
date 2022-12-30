f0825adc4c67c10afaac866c633c92c364d08097
Batuhan <Batuhan@BATU-NOTEBOOK>
2022-12-30 12:24:51 +0300

    fix error

--- MQTTconsoleapp/Program.cs
+++ MQTTconsoleapp/Program.cs
@@ -11,12 +11,8 @@ namespace MQTTconsoleapp
     {
         static async Task Main(string[] args)
         {
-            byte[] payload = Encoding.UTF8.GetBytes("Yeni Mesaj");
-            dynamic variable = new JObject();
-            variable.value = 4;
             var factory = new MqttFactory();
             var mqttClient = factory.CreateMqttClient();
-            var responsetopic = $"v1/devices/me/rpc/response/{variable.value}";
             var options = new MqttClientOptionsBuilder()
                 .WithTcpServer("cloud.thingson.io", 1883)
                 .WithKeepAliveSendInterval(TimeSpan.FromSeconds(300)).WithClientId("Client1")
@@ -24,31 +20,23 @@ namespace MQTTconsoleapp
                 .WithCleanSession(false).Build();
             await mqttClient.ConnectAsync(options);
             Console.WriteLine("Connection: "+mqttClient.IsConnected);
-
-
-            var requesttopic = $"v1/devices/me/rpc/request/{variable.value}";
-
-            var subs=await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic(responsetopic).WithAtMostOnceQoS().Build());
+            string requesttopic = $"v1/devices/me/rpc/request/+";
+            var subs=await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic(requesttopic).Build());
             Console.WriteLine(subs.Items[0].ResultCode);
-            mqttClient.UseApplicationMessageReceivedHandler(e =>
+            Console.WriteLine(subs.Items[0].TopicFilter);
+            mqttClient.UseApplicationMessageReceivedHandler(async e =>
             {
-                Console.WriteLine(Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+
+                var msg = JObject.Parse(Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+                string mesaj = msg["params"].ToString();
+                Console.WriteLine(msg);
+                Console.WriteLine(mesaj);
+                string requesttopicnow = e.ApplicationMessage.Topic;
+                dynamic value=(requesttopicnow.Split("/").Last());
+                await mqttClient.PublishAsync($"v1/devices/me/rpc/response/{value}",mesaj);
             });
-            
-              //  var msg = JObject.Parse(Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
-                //string message = msg["params"].ToString();
-                //Console.WriteLine(message);
-            
-            
             Console.ReadLine();
-            Console.WriteLine(mqttClient.IsConnected);
             
-            
-            
-            
-            
-
-
         }
     }
 }
